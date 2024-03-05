2afaedb4deef3bd185af457109dad69f77e9c312
devinsharris99 <156541918+devinsharris99@users.noreply.github.com>
2024-03-04 18:27:18 -0600

    Add files via upload
    New Directory for desktop app (for easier access). Desktop Logic is now simplified and has added modularity for any page we want to implement (as we've stated in the proposal before). Next step is firebase inclusion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,37 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
+    kotlin("jvm") version "1.9.22" 
+    id("org.jetbrains.compose") version "1.6.0"
+}
+
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "DesktopApplication.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "CapstoneDesktopApp"
+            packageVersion = "1.0.0"
+        }
+    }
 }
---
+++ src/main/kotlin/User.kt
@@ -0,0 +1,7 @@
+package DesktopApplication
+
+data class User(
+    var id: String? = null,
+    var name: String? = null,
+    var email: String? = null
+)
