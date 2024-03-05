2ac1d377b75ca9d504e5bb67c2c87590c35a7c9f
devinsharris99 <156541918+devinsharris99@users.noreply.github.com>
2024-03-04 19:06:08 -0600

    Add files via upload

---
+++ DesktopApplication/src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+//@file:OptIn(ExperimentalResourceApi::class)
+
+package DesktopApplication
+
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+//import org.jetbrains.compose.resources.ExperimentalResourceApi
+
+/*fun initializeFirebase() {
+    val serviceAccount = FileInputStream("path/to/google-services.json")
+
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl("https://console.firebase.google.com/u/0/project/haz-mat-7ad0f/database/haz-mat-7ad0f-default-rtdb/data")
+        .build()
+
+    FirebaseApp.initializeApp(options)
+}*/
+
+fun main() = application {
+
+    //initializeFirebase()
+
+    Window(onCloseRequest = ::exitApplication, title = "UARK HAZMat Capstone") {
+        AppContent()
+    }
+}
+
+@Preview
+@Composable
+fun AppDesktopPreview() {
+    AppContent()
+}
