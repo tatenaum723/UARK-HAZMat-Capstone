6ba134ddfc8c942304930ed6665323746f6c217b
tatenaumann <tate.naumann1@gmail.com>
2024-04-01 20:25:46 -0500

    Semi-working timed line graph

--- composeApp/src/androidMain/app/src/main/java/com/example/hazmatapp/View/GraphCapture.kt
+++ composeApp/src/androidMain/app/src/main/java/com/example/hazmatapp/View/GraphCapture.kt
@@ -1,30 +1,145 @@
 package com.example.hazmatapp.View
 
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.hazmatapp.R
-import EmulatorDataListener
-import EmulatorUtil
-import android.content.Intent
-import android.util.Log
-import android.widget.Button
-import android.widget.ProgressBar
+import android.view.WindowManager
+import android.widget.SeekBar
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hazmatapp.R
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.Random
+import java.util.concurrent.TimeUnit
+
 
 class GraphCapture : AppCompatActivity() {
+
+    private lateinit var chart: LineChart
+    private lateinit var seekBarX: SeekBar
+    private lateinit var tvX: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_graph_capture)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        title = "LineChartTime"
+
+        tvX = findViewById(R.id.tvXMax)
+        seekBarX = findViewById(R.id.seekBar1)
+
+        chart = findViewById(R.id.chart1)
+
+        // no description text
+        chart.description.isEnabled = false
+
+        // enable touch gestures
+        chart.setTouchEnabled(true)
+
+        chart.dragDecelerationFrictionCoef = 0.9f
+
+        // enable scaling and dragging
+        chart.isDragEnabled = true
+        chart.setScaleEnabled(true)
+        chart.setDrawGridBackground(false)
+        chart.isHighlightPerDragEnabled = true
+
+        // set an alternative background color
+        chart.setBackgroundColor(Color.WHITE)
+        chart.setViewPortOffsets(0f, 0f, 0f, 0f)
+
+        // add data
+        seekBarX.progress = 100
+
+        // get the legend (only possible after setting data)
+        val l = chart.legend
+        l.isEnabled = false
+
+        val xAxis = chart.xAxis
+        xAxis.position = XAxis.XAxisPosition.TOP_INSIDE
+        xAxis.textSize = 10f
+        xAxis.textColor = Color.WHITE
+        xAxis.setDrawAxisLine(false)
+        xAxis.setDrawGridLines(true)
+        xAxis.textColor = Color.rgb(255, 192, 56)
+        xAxis.setCenterAxisLabels(true);
+        xAxis.granularity = 1f // one hour
+        object : IAxisValueFormatter {
+            private val mFormat = SimpleDateFormat("dd MMM HH:mm", Locale.ENGLISH)
+
+            @Deprecated("Deprecated in Java")
+            override fun getFormattedValue(value: Float, axis: AxisBase): String {
+                val millis = TimeUnit.HOURS.toMillis(value.toLong())
+                return mFormat.format(Date(millis))
+            }
         }
+
+        val leftAxis = chart.axisLeft
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+        leftAxis.textColor = ColorTemplate.getHoloBlue()
+        leftAxis.setDrawGridLines(true)
+        leftAxis.isGranularityEnabled = true
+        leftAxis.axisMinimum = 0f
+        leftAxis.axisMaximum = 170f
+        leftAxis.yOffset = -9f
+        leftAxis.textColor = Color.rgb(255, 192, 56)
+
+        val rightAxis = chart.axisRight
+        rightAxis.isEnabled = false
+
+        setData(5, 10.0)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun setData(count: Int, range: Double) {
+        // now in hours
+        val now = TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis())
+
+        val values = ArrayList<Entry>()
+
+        // count = hours
+        val to = now + count
+
+        // increment by 1 hour
+        for (x in now until to) {
+            val y = Random().nextFloat() * range + Float.MIN_VALUE
+            values.add(Entry(x.toFloat(), y.toFloat())) // add one entry per hour
+        }
+
+        // create a dataset and give it a type
+        val set1 = LineDataSet(values, "DataSet 1")
+        set1.axisDependency = YAxis.AxisDependency.LEFT
+        set1.color = ColorTemplate.getHoloBlue()
+        set1.valueTextColor = ColorTemplate.getHoloBlue()
+        set1.lineWidth = 1.5f
+        set1.setDrawCircles(false)
+        set1.setDrawValues(false)
+        set1.fillAlpha = 65
+        set1.fillColor = ColorTemplate.getHoloBlue()
+        set1.highLightColor = Color.rgb(244, 117, 117)
+        set1.setDrawCircleHole(false)
+
+        // create a data object with the data sets
+        val data = LineData(set1)
+        data.setValueTextColor(Color.WHITE)
+        data.setValueTextSize(9f)
+
+        // set data
+        chart.data = data
     }
 }
